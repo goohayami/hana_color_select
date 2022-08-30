d9ca8abad3dfe7e3e74b6c2ba24673b9cfb7b21a
goohayami <98527002+goohayami@users.noreply.github.com>
2022-08-30 20:37:43 +0900

    Add files via upload

---
+++ src/zensou.js
@@ -0,0 +1,132 @@
+// 背景
+const red = document.getElementById("red");
+const blue = document.getElementById("blue");
+const yellow = document.getElementById("yellow");
+const grern = document.getElementById("grern");
+const purple = document.getElementById("purple");
+const brown = document.getElementById("brown");
+const white = document.getElementById("white");
+
+red.addEventListener("click", () => {
+  document.body.style.backgroundColor = "#ea5550";
+});
+blue.addEventListener("click", () => {
+  document.body.style.backgroundColor = "#82cddd";
+});
+yellow.addEventListener("click", () => {
+  document.body.style.backgroundColor = "#fff462";
+});
+grern.addEventListener("click", () => {
+  document.body.style.backgroundColor = "#79c06e";
+});
+purple.addEventListener("click", () => {
+  document.body.style.backgroundColor = "#c7a5cc";
+});
+brown.addEventListener("click", () => {
+  document.body.style.backgroundColor = "#98605e";
+});
+white.addEventListener("click", () => {
+  document.body.style.backgroundColor = "#fafdff";
+});
+// 大文字
+const redO = document.getElementById("redO");
+const blueO = document.getElementById("blueO");
+const yellowO = document.getElementById("yellowO");
+const grernO = document.getElementById("grernO");
+const purpleO = document.getElementById("purpleO");
+const brownO = document.getElementById("brownO");
+const whiteO = document.getElementById("whiteO");
+
+redO.addEventListener("click", () => {
+  const alls = document.querySelectorAll(".big");
+  alls.forEach((all) => {
+    all.style.backgroundColor = "#ea5550";
+  });
+});
+blueO.addEventListener("click", () => {
+  const alls = document.querySelectorAll(".big");
+  alls.forEach((all) => {
+    all.style.backgroundColor = "#82cddd";
+  });
+});
+yellowO.addEventListener("click", () => {
+  const alls = document.querySelectorAll(".big");
+  alls.forEach((all) => {
+    all.style.backgroundColor = "#fff462";
+  });
+});
+grernO.addEventListener("click", () => {
+  const alls = document.querySelectorAll(".big");
+  alls.forEach((all) => {
+    all.style.backgroundColor = "#79c06e";
+  });
+});
+purpleO.addEventListener("click", () => {
+  const alls = document.querySelectorAll(".big");
+  alls.forEach((all) => {
+    all.style.backgroundColor = "#c7a5cc";
+  });
+});
+brownO.addEventListener("click", () => {
+  const alls = document.querySelectorAll(".big");
+  alls.forEach((all) => {
+    all.style.backgroundColor = "#98605e";
+  });
+});
+whiteO.addEventListener("click", () => {
+  const alls = document.querySelectorAll(".big");
+  alls.forEach((all) => {
+    all.style.backgroundColor = "#fafdff";
+  });
+});
+
+const redK = document.getElementById("redK");
+const blueK = document.getElementById("blueK");
+const yellowK = document.getElementById("yellowK");
+const grernK = document.getElementById("grernK");
+const purpleK = document.getElementById("purpleK");
+const brownK = document.getElementById("brownK");
+const whiteK = document.getElementById("whiteK");
+
+redK.addEventListener("click", () => {
+  const alls = document.querySelectorAll(".sml");
+  alls.forEach((all) => {
+    all.style.backgroundColor = "#ea5550";
+  });
+});
+blueK.addEventListener("click", () => {
+  const alls = document.querySelectorAll(".sml");
+  alls.forEach((all) => {
+    all.style.backgroundColor = "#82cddd";
+  });
+});
+yellowK.addEventListener("click", () => {
+  const alls = document.querySelectorAll(".sml");
+  alls.forEach((all) => {
+    all.style.backgroundColor = "#fff462";
+  });
+});
+grernK.addEventListener("click", () => {
+  const alls = document.querySelectorAll(".sml");
+  alls.forEach((all) => {
+    all.style.backgroundColor = "#79c06e";
+  });
+});
+purpleK.addEventListener("click", () => {
+  const alls = document.querySelectorAll(".sml");
+  alls.forEach((all) => {
+    all.style.backgroundColor = "#c7a5cc";
+  });
+});
+brownK.addEventListener("click", () => {
+  const alls = document.querySelectorAll(".sml");
+  alls.forEach((all) => {
+    all.style.backgroundColor = "#98605e";
+  });
+});
+whiteK.addEventListener("click", () => {
+  const alls = document.querySelectorAll(".sml");
+  alls.forEach((all) => {
+    all.style.backgroundColor = "#fafdff";
+  });
+});
